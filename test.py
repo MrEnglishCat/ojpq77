a1ca155007fc3ccba80b6521335102acb8b8eb5d
Mr. EnglishCat <mr.englishcat@gmail.com>
2025-08-05 20:52:18 +0300

    all_end

--- test.py
+++
@@ -1,8 +0,0 @@
-from mimesis import Datetime, Text
-
-print(Datetime(locale="ru").datetime().strftime('%Y-%m-%d_%H:%M'))
-text = Text(locale="ru").text(quantity=1)
-if len(text) > 50:
-    print(f"{text[:47]}...")
-else:
-    print(f"{text}")
